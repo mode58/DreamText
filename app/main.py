c1cfca84a8fe020ce089fe9bb59505b477d4bb4d
mode58 <yuta.yamaguchi0216@outlook.jp>
2025-05-01 18:38:26 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import google.generativeai as genai
 import os
 import sys
 from dotenv import load_dotenv
-from datetime import datetime
+from datetime import datetime,timedelta
 
 
 # .envファイルから環境変数を読み込む
@@ -14,22 +14,35 @@ if not api_key:
     print("APIキーが見つかりません")
     sys.exit()
 
-
 genai.configure(api_key = api_key)
 
-# モデルを選択
+# モデルを指定
 model = genai.GenerativeModel(model_name='gemini-1.5-pro-002')
 
-#dreamcontent = "地面を掘っている夢を見ました"
+#過去何日分の夢を読み取るか
+DAYS_count = 3
+#今日の日付からカウント分を引く
+today = datetime.now()
+cut_date = today - timedelta(days=DAYS_count)
+
+#日付だけにする
+format_today = today.strftime("%Y年%m月%d日 %H:%M")
+
+#過去の夢の読み込み
+try:
+    with open('Dream_log.txt', 'r',encoding='utf-8') as f:
+        past_data = f.read()
+except FileNotFoundError:
+    with open("Dream_log.tet","w", encoding= "utf-8") as f:
+        f.write('')
+    print("ファイルを作成しました")
+
 dreamcontent = input("見た夢の内容を教えてください: ")
-Preface = "夢を分析して心理状態を測ってください"
-prompt = Preface + dreamcontent
+Preface = "今回の夢と過去の夢を分析して心理状態を測ってください"
+prompt = Preface + dreamcontent + past_data
 
 response = model.generate_content(prompt)
 interpretation = response.text #解釈結果のテキストを取得
-
-today = datetime.now() #今日の日付取得
-format_today = today.strftime("%Y年%m月%d日 %H:%M")  #日付だけにする
 
 try:
     with open('Dream_log.txt', 'a', encoding='utf-8') as f:
